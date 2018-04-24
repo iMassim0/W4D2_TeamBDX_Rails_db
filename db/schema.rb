cd04cc43a3de7cc0343f8d1052607a2170d430bd
Baptiste Rogeon <baptiste.rogeon@gmail.com>
2018-04-24 16:01:26 +0200

    h

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_04_24_130627) do
+ActiveRecord::Schema.define(version: 2018_04_24_134618) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
@@ -33,6 +33,8 @@ ActiveRecord::Schema.define(version: 2018_04_24_130627) do
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
