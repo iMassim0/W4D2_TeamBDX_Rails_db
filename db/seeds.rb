4c365a63f8972adf42e7fed460280aeed69797b9
Baptiste Rogeon <baptiste.rogeon@gmail.com>
2018-04-24 18:00:57 +0200

    zfesfse

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,19 +1,10 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
 require 'faker'
-i = 1
-20.times do |i|
-   # User.create(name: Faker::Name.unique.name)
-   @truc = User.id(i+1)
-   temp = Post.new(title: Faker::Lorem.word, body: Faker::Lorem.paragraph, description: Faker::Lorem.sentence, user_id: @truc)
-   temp.save
 
-end
+100.times do
+   wesh = User.create(name: Faker::Name.unique.name)
+   post = Post.create(title: Faker::Lorem.word, body: Faker::Lorem.paragraph, description: Faker::Lorem.sentence, user_id: Faker::Number.between(1, 120))
+ end
+
 
 # cats = ["sport", "musique", "cinema", "technologie", "internet", "politique", "actualite"]
 # cats.each do |cat|
