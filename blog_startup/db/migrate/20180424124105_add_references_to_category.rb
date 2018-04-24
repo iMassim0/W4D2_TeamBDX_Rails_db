cbf90854a96546378020617c533ee12fd4f987a0
Baptiste Rogeon <baptiste.rogeon@gmail.com>
2018-04-25 00:10:14 +0200

    last

--- blog_startup/db/migrate/20180424124105_add_references_to_category.rb
+++
@@ -1,5 +0,0 @@
-class AddReferencesToCategory < ActiveRecord::Migration[5.2]
-  def change
-      add_reference(:categories, :post)
-  end
-end
--- blog_startup/db/migrate/20180424134618_add_user_id_to_post_table.rb
+++
@@ -1,7 +0,0 @@
-class AddUserIdToPostTable < ActiveRecord::Migration[5.2]
-  def change
-    change_table :posts do |t|
-      t.references :user 
-    end
-  end
-end
